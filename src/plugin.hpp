2b879cad12d16ba71b510369a35f892cba82ad1b
davekch <dave-koch@web.de>
2020-05-10 16:48:13 +0200

    add slider to gchoice

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -10,3 +10,12 @@ extern Plugin* pluginInstance;
 // Declare each Model, defined in each module source file
 extern Model* modelUchoice;
 extern Model* modelGchoice;
+
+struct LargeSlider : SvgSlider {
+	LargeSlider() {
+		maxHandlePos = app::mm2px(Vec(-2.457, 0.738));
+		minHandlePos = app::mm2px(Vec(-2.457, 79.40));
+		this->setBackgroundSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sliderbackground.svg")));
+        this->setHandleSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sliderhandle.svg")));
+	}
+};
