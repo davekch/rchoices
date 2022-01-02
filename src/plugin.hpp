0a84df93acd754c9656f735475049b162176ca45
davekch <dave-koch@web.de>
2022-01-02 20:23:18 +0100

    migrate to rack version 2

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -13,8 +13,8 @@ extern Model* modelGchoice;
 
 struct LargeSlider : SvgSlider {
 	LargeSlider() {
-		maxHandlePos = app::mm2px(Vec(-2.2128, 0.738));
-		minHandlePos = app::mm2px(Vec(-2.2128, 79.40));
+		maxHandlePos = window::mm2px(Vec(-2.2128, 0.738));
+		minHandlePos = window::mm2px(Vec(-2.2128, 79.40));
 		this->setBackgroundSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sliderbackground.svg")));
         this->setHandleSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sliderhandle.svg")));
 	}
